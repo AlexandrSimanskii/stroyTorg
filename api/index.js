b54bdd033669f2e04db60f59487c837561d65c60
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2024-06-12 18:18:24 +0200

    ghj

--- api/index.js
+++ api/index.js
@@ -9,10 +9,7 @@ import authRouter from "./routes/auth.router.js";
 import usersRouter from "./routes/users.router.js";
 import productRouterDB from "./routes/productDB.router.js";
 import authRouterDB from "./routes/authDB.router.js";
-import path from "path";
 
-
-const __dirname = path.resolve();
 
 const app = express();
 
@@ -25,29 +22,12 @@ mongoose
   })
   .catch((err) => console.log("Не удалось подключиться к MongoDB", err));
 const PORT = process.env.PORT || 3004;
-const allowedOrigins = [
-  process.env.CORS_ORIGIN || "http://193.222.62.100",
-  "https://vercel.com/alexs-projects-f142ab41/mebel-front",
-];
+
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3004");
 });
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
 
 
@@ -61,10 +41,7 @@ app.use("/api/review", reviewRouter);
 app.use("/api/productsDB", productRouterDB);
 app.use("/api/authDB", authRouterDB);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.json(__dirname, "client", "dist", "index.html"));
-});
+
 
 
 app.use((err, req, res, next) => {
